4b6b934aa14e602134602efc50a1b4d1725c726d
Dev_NIX <devnix.code@gmail.com>
2020-10-30 13:09:11 +0100

    Update TinyInt.php

--- TinyInt.php
+++ TinyInt.php
@@ -1,18 +1,53 @@
 <?php
 
-namespace GollumSF\Doctrine;
+namespace Devnix\Doctrine\DBAL\Types;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
 
 class TinyInt extends Type {
-	const TINYINT = 'tinyint';
-	
-	public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) {
-		return 'TINYINT' . ( ! empty($fieldDeclaration['unsigned']) ? ' UNSIGNED' : '');
-	}
-	
-	public function getName() {
-		return self::TINYINT;
-	}
+    const TINYINT = 'tinyint';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        $fieldDeclaration = array_merge([
+            'length' => 1,
+        ], $fieldDeclaration);
+
+        $sqlDeclaration =  sprintf("TINYINT(%d)",
+            $fieldDeclaration['length']
+        );
+
+        if (!empty($fieldDeclaration['unsigned'])) {
+            $sqlDeclaration .= ' UNSIGNED';
+        }
+
+
+        return $sqlDeclaration;
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return false;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return (int) $value;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return (int) $value;
+    }
+
+    public function getName()
+    {
+        return self::TINYINT;
+    }
+
+    public function getBindingType()
+    {
+        return \PDO::PARAM_INT;
+    }
 }
